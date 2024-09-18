cfb820476e208ee04ffdd29cfc87525c4b7c8849
Yashwanth Reddy <yashwanth.yenugu@gmail.com>
2024-09-18 17:57:46 +0530

    Integrated Passport Authentication

--- src/auth/decorators/skipAuth.ts
+++ src/auth/decorators/skipAuth.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const SKIP_AUTH_KEY = 'skipAuth';
-export const SkipAuth = () => SetMetadata(SKIP_AUTH_KEY, true);
+export const IS_SKIP_AUTH_KEY = 'isPublic';
+export const skipAuth = () => SetMetadata(IS_SKIP_AUTH_KEY, true);
---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,22 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_SKIP_AUTH_KEY } from './decorators/skipAuth';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(
+      IS_SKIP_AUTH_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}
