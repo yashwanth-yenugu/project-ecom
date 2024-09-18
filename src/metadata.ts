1941964910e716ccd09a50f647129f27a7f0d0bf
Yashwanth Reddy <yashwanth.yenugu@gmail.com>
2024-09-18 16:59:50 +0530

    Metadata updated (auto generated)

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,33 +1,5 @@
 /* eslint-disable */
 export default async () => {
-  const t = {};
-  return {
-    '@nestjs/swagger': {
-      models: [
-        [
-          import('./auth/models/signIn.dto'),
-          {
-            SignInDto: {
-              username: { required: true, type: () => String, example: 'yash' },
-              password: {
-                required: true,
-                type: () => String,
-                example: 'admin',
-              },
-            },
-          },
-        ],
-      ],
-      controllers: [
-        [
-          import('./app.controller'),
-          { AppController: { getHello: { type: String } } },
-        ],
-        [
-          import('./auth/auth.controller'),
-          { AuthController: { signIn: {}, getProfile: { type: Object } } },
-        ],
-      ],
-    },
-  };
+    const t = {};
+    return { "@nestjs/swagger": { "models": [[import("./auth/models/signIn.dto"), { "SignInDto": { username: { required: true, type: () => String, example: "yash" }, password: { required: true, type: () => String, example: "admin" } } }]], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { type: String } } }], [import("./auth/auth.controller"), { "AuthController": { "signIn": {}, "getProfile": { type: Object } } }]] } };
 };
